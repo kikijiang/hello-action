9c5ef5367d135cabbf64d12664b90afdc4fb8a1d
辰奂 <heyanjiao.hyj@antgroup.com>
2023-03-10 11:47:13 +0800

    fix: 完善 action todo

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const github = require('@actions/github');
 
 /**
  * 1. input：小程序应用 ID，API 调用的身份信息
- * 2. 内部逻辑：调用行星 API
+ * 2. 内部逻辑：
+ *  2.0 调用 github api，创建 pr 的评论，评论内容是 loading 状态
+ *  2.1 调用行星 API，获取阿里云 OSS 的临时访问凭证
+ *  2.2 调用阿里云 nodejs 的 SDK 上传本地文件到阿里云 OSS
+ *  2.3 根据 pull_request 的类型，判断是否是第一次创建 pr，调用不同的行星 API（或者传不同的参数）
+ *  2.4 调用行星轮询接口，直到获取到预览码，调用 github api 更新预览码评论
  * 3. output：暂无
  */
 
