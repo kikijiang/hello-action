f7955fb7585b6b0b5a7b8936a502ef5175811a4b
辰奂 <heyanjiao.hyj@antgroup.com>
2023-03-12 15:45:03 +0800

    fix: support artifact to oss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { api_public_key, api_private_key } = require('./key')
+const { api_public_key } = require('./key')
 
 /**
  * 1. input：小程序应用 ID，API 调用的身份信息
@@ -18,14 +18,51 @@ const { api_public_key, api_private_key } = require('./key')
 try {
   const appId = core.getInput('appId');
   const apiSecretKey = core.getInput('apiSecretKey');
+  const artifact = core.getInput('artifact');
   console.log(`Hello ${appId}!`);
   console.log("=======", api_public_key)
+  console.log("=======", artifact)
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  // 1. 网关私钥可以通过 variable 传递
   console.log("secret key:", apiSecretKey);
   const encrptStr = crypto.publicEncrypt(api_public_key, "I'm KIKI");
   const nativeStr = crypto.privateDecrypt(apiSecretKey, encrptStr);
-  console.log("🎉解密成功啦", nativeStr)
+  console.log("🎉解密成功啦", nativeStr.toString())
+
+  // 2. 使用临时凭证上传 artifact
+  const axios = require("axios");
+  const OSS = require("ali-oss");
+
+  // 在客户端使用临时访问凭证初始化OSS客户端，用于临时授权访问OSS资源。
+  const getToken = async () => {
+    // 设置客户端请求访问凭证的地址。
+    await Promise.resolve({
+      data:{
+        "SecurityToken": "CAIS8wF1q6Ft5B2yfSjIr5bQH8n6qrFX8JOhbHX71noDTuhpjYfS0Tz2IHBEf3hrAu8at/k/m2BS6vcclqNpQppCXlfYYNBstnHYL8wkO9ivgde8yJBZor/HcDHhJnyW9cvWZPqDP7G5U/yxalfCuzZuyL/hD1uLVECkNpv74vwOLK5gPG+CYCFBGc1dKyZ7tcYeLgGxD/u2NQPwiWeiZygB+CgE0D8jsfjlmpDMtEWC1QOlktV4/dqhfsKWCOB3J4p6XtuP2+h7S7HMyiY46WIRpPct3fcfpmue44HCUwMNskucUfDd99p0NxN0fbQq1Xm1L3BVOpcagAFtyMpZ0HuEg28pSCs3aBqFzPu6yA6cNmxaVpAyZpdQewQwPdB43xOvwnIJcKlcKySNsT5s7i5g0idhfDe2VHCEmCOt4Aukvp5jrVMC6dHigQ1aGrXQH2fIUEd+XOZWK0hgRRzQJNVHnx/eUGDYXDGUMtiSoWqqas+hRHcNVfiVDw==",
+        "AccessKeyId": "STS.NUeTsNGnvGQJnSJ2zVBgEbBy3",
+        "AccessKeySecret": "7drifsyDHJMRh4Ym5PBEKaSXRA55PUdbVp2CsB1WLPsL",
+      }
+    }).then((token) => {
+      const client = new OSS({
+        // yourRegion填写Bucket所在地域。以华东1（杭州）为例，yourRegion填写为oss-cn-hangzhou。
+        region: 'oss-cn-hangzhou',
+        accessKeyId: token.data.AccessKeyId,
+        accessKeySecret: token.data.AccessKeySecret,
+        stsToken: token.data.SecurityToken,
+        // 填写Bucket名称。
+        bucket: "githubforkiki",
+      });
+      // 使用临时访问凭证上传文件。
+      // 填写不包含Bucket名称在内的Object的完整路径，例如exampleobject.jpg。
+      // 填写本地文件的完整路径，例如D:\\example.jpg。
+      client.put('miniprogram', artifact).then((res)=>{
+        console.log("🎉上传成功", res)
+      }).catch(e=>console.log(e))
+    });
+  };
+
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
