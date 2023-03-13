e4410557b5c183d05b43127d64fa6b91be1773f7
辰奂 <heyanjiao.hyj@antgroup.com>
2023-03-13 15:47:59 +0800

    fix: oss upload

--- dist/index.js
+++ dist/index.js
@@ -96866,7 +96866,7 @@ try {
       // 使用临时访问凭证上传文件。
       // 填写不包含Bucket名称在内的Object的完整路径，例如exampleobject.jpg。
       // 填写本地文件的完整路径，例如D:\\example.jpg。
-      client.put('miniprogram', artifact).then((res)=>{
+      client.put(`${appId}.tgz`, artifact).then((res)=>{
         console.log("🎉上传成功", res)
       }).catch(e=>console.log(e))
     })
